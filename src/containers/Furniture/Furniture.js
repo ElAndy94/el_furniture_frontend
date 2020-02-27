6e93e753d4515f36f7866254d8ed63167f67d2b6
Andrew <andrewpeliza@hotmail.com>
2020-02-27 11:04:49 +0000

    We can update cms items via react, about us next.

--- src/containers/Furniture/Furniture.js
+++ src/containers/Furniture/Furniture.js
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
 import FurnitureItem from '../../components/FurnitureItem/FurnitureItem';
 import './Furniture.scss';
 import config from '../../config/config';
-const contentful = require('contentful');
+import { createClient } from 'contentful-management';
 
 function Furniture() {
   const [furniture, setFurniture] = useState([]);
 
   useEffect(() => {
-    let client = contentful.createClient({
-      space: config.SPACE_ID,
+    const client = createClient({
       accessToken: config.ACCESS_TOKEN
     });
-    client.getEntries({ content_type: 'furniture' }).then(res => {
-      setFurniture(res.items);
-    });
 
-    client.getEntry('58lxxeTop1mFMcBjiEhlWq').then(res => {
-      console.log(res);
+    client.getSpace(config.SPACE_ID).then(space => {
+      // space.getEntry('58lxxeTop1mFMcBjiEhlWq').then(item => {
+      // item.fields.name['en-US'] = 'Saragosa';
+      // return item.update();
+      // });
+      space
+        .getEntries()
+        .then(res => {
+          setFurniture(res.items);
+        })
+        .catch(console.error);
     });
+    // client.getEntries({ content_type: 'furniture' }).then(res => {
+    //   setFurniture(res.items);
+    // });
   }, []);
 
   let furnitureItems = furniture.map((item, i) => {
-    // console.log(item);
     return (
       <FurnitureItem
         key={i}
         itemId={item.sys.id}
-        name={item.fields.name}
-        type={item.fields.type}
-        price={item.fields.price}
-        dimensions={item.fields.dimensions}
-        location={item.fields.location}
-        quantity={item.fields.quantity}
+        name={item.fields.name['en-US']}
+        type={item.fields.type['en-US']}
+        price={item.fields.price['en-US']}
+        dimensions={item.fields.dimensions['en-US']}
+        location={item.fields.location['en-US']}
+        quantity={item.fields.quantity['en-US']}
       />
     );
   });
